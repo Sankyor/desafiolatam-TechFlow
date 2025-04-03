1f4e1d3cc07cc0495e85d4c93c191498054fbb45
Mauricio Moraga <mmoraga.m3@gmail.com>
2025-04-03 20:02:15 -0300

    feat:(stable-v1)

---
+++ Jenkinsfile
@@ -0,0 +1,25 @@
+pipeline {
+  agent any
+  stages {
+    stage('Clonar Repo') {
+      steps {
+        git url: 'https://github.com/Sankyor/desafiolatam-TechFlow.git', branch: 'main'
+      }
+    }
+    stage('Instalar Dependencias') {
+      steps {
+        sh 'npm install' // Cambia de 'npm ci' a 'npm install'
+      }
+    }
+    stage('Pruebas') {
+      steps {
+        sh 'npm run test'
+      }
+    }
+    stage('Construir Docker') {
+      steps {
+        sh 'docker build -t TechFlow-api .'
+      }
+    }
+  }
+}
--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const data = require('./db.json');
+const data = require("./db.json");
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
   res.status(200).json(data.users);
 });
 
-app.get('/users/:id', (req, res) => {
-  const user = data.users.find(u => u.id === parseInt(req.params.id, 10));
+app.get("/users/:id", (req, res) => {
+  const user = data.users.find((u) => u.id === parseInt(req.params.id, 10));
   if (user) {
     res.status(200).json(user);
   } else {
-    res.status(404).json({ error: 'User not found' });
+    res.status(404).json({ error: "User not found" });
   }
 });
 
 module.exports = app;
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`API is running on port ${PORT}`));
